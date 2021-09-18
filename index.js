ce061aa308dcacba26c76651e6a45502f90e550f
manav-1 <manav81101@gmail.com>
2021-09-18 20:04:34 +0530

    env variables added properly

--- index.js
+++ index.js
@@ -268,18 +268,7 @@ app.post("/login", (req, res) => {
         res.send(error);
       });
   } catch (err) {
-    res.send({
-      abc: {
-        apiKey: process.env.apiKey,
-        authDomain: process.env.authDomain,
-        databaseURL: process.env.databaseURL,
-        projectId: process.env.projectId,
-        storageBucket: process.env.storageBucket,
-        messagingSenderId: process.env.messagingSenderId,
-        appId: process.env.appId,
-      },
-      err,
-    });
+    res.send("Internal Error");
   }
 });
 
